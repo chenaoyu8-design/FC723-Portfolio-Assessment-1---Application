6d6aeced90a0ad95cb00f43c7c972e76a694d428
chenaoyu8-design <chenaoyu8@gmail.com>
2026-03-29 01:13:06 +0800

    Update gcd_calculator.py

--- gcd_calculator.py
+++ gcd_calculator.py
@@ -9,8 +9,26 @@ class EuclideanAlgorithm:
             b = remainder
         return a
 
-#function test
+    def run_app(self):
+        print("GCD Calculator")
+        
+        #simple tap in
+        val1 = input("please enter the first number")
+        val2 = input("please enter the second number")
+        
+        #check and make sure only entering number
+        if val1.isdigit() and val2.isdigit():
+            num1 = int(val1)
+            num2 = int(val2)
+            
+            if num1 > 0 and num2 >0:
+                result = self.calculate_gcd(num1, num2)
+                print(f"The Greatest Common Divisor is: {result}")
+            else:
+                print("please enter number greater than zero. ")
+        else:
+            print("Invalid input! Please enter whole numbers only.")
 if __name__ == "__main__":
     solver = EuclideanAlgorithm()
-    #input number to test 
-    print(f"test result (48,18):{solver.calculate_gcd(48,18)}")
+    solver.run_app()
+
